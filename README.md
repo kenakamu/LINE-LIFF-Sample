9e4b1c3829f2a03eaaf1e73fc773dddd2fa235b7
Kenichiro Nakamura <kenakamu@microsoft.com>
2018-07-10 13:06:32 +0900

    initial

---
+++ README.md
@@ -0,0 +1,14 @@
+# Line Front-end Framework サンプル
+
+このサンプルは LINE Developer Bootcamp - LIFF 開発入門 で使ったサンプルです。またコードの多くは [LINE LIFF Starter](https://github.com/line/line-liff-starter) より拝借しています。
+
+解説は [LINE の LIFF 開発超入門 : LIFF SDK を使った Web アプリの開発](https://qiita.com/kenakamu/private/44cba247ab51b6c5ed51) を参照してください。
+
+# 参考
+[公式ドキュメント: LINE Front-end Framework](https://developers.line.me/ja/docs/liff/)
+[公式ブログ: Messaging APIの新機能LIFFの使い方を解説します。](https://engineering.linecorp.com/ja/blog/detail/299)
+
+## Thanks to
+Hirotada Kobayashi [pierre3](https://github.com/pierre3/)
+Shoya Shiraki [morugu](https://github.com/morugu)
+Kazuki Nakajima [nkjm](nkjm)
---
+++ liff.js
@@ -0,0 +1,78 @@
+window.onload = function (e) {
+    // init で初期化。基本情報を取得。
+    // https://developers.line.me/ja/reference/liff/#initialize-liff-app
+    liff.init(function (data) {
+        getProfile();
+        initializeApp(data);
+    });
+
+    // LIFF アプリを閉じる
+    // https://developers.line.me/ja/reference/liff/#liffclosewindow()
+    document.getElementById('closewindowbutton').addEventListener('click', function () {
+        liff.closeWindow();
+    });
+
+    // ウィンドウを開く
+    // https://developers.line.me/ja/reference/liff/#liffopenwindow()
+    document.getElementById('openwindowbutton').addEventListener('click', function () {
+        liff.openWindow({
+            url: 'https://line.me'
+        });
+    });
+
+    document.getElementById('openwindowexternalbutton').addEventListener('click', function () {
+        liff.openWindow({
+            url: 'https://line.me',
+            external: true
+        });
+    });
+
+    // メッセージの送信
+    document.getElementById('sendmessagebutton').addEventListener('click', function () {
+        https://developers.line.me/ja/reference/liff/#liffsendmessages()
+        liff.sendMessages([{
+            type: 'text',
+            text: "テキストメッセージの送信"
+        }, {
+            type: 'sticker',
+            packageId: '2',
+            stickerId: '144'
+        }]).then(function () {
+            window.alert("送信完了");
+        }).catch(function (error) {
+            window.alert("Error sending message: " + error);
+        });
+    });
+};
+
+// プロファイルの取得と表示
+function getProfile(){
+    // https://developers.line.me/ja/reference/liff/#liffgetprofile()
+    liff.getProfile().then(function (profile) {
+        document.getElementById('useridprofilefield').textContent = profile.userId;
+        document.getElementById('displaynamefield').textContent = profile.displayName;
+
+        var profilePictureDiv = document.getElementById('profilepicturediv');
+        if (profilePictureDiv.firstElementChild) {
+            profilePictureDiv.removeChild(profilePictureDiv.firstElementChild);
+        }
+        var img = document.createElement('img');
+        img.src = profile.pictureUrl;
+        img.alt = "Profile Picture";
+        img.width = 200;
+        profilePictureDiv.appendChild(img);
+
+        document.getElementById('statusmessagefield').textContent = profile.statusMessage;
+    }).catch(function (error) {
+        window.alert("Error getting profile: " + error);
+    });
+}
+
+function initializeApp(data) {
+    document.getElementById('languagefield').textContent = data.language;
+    document.getElementById('viewtypefield').textContent = data.context.viewType;
+    document.getElementById('useridfield').textContent = data.context.userId;
+    document.getElementById('utouidfield').textContent = data.context.utouId;
+    document.getElementById('roomidfield').textContent = data.context.roomId;
+    document.getElementById('groupidfield').textContent = data.context.groupId;
+}
